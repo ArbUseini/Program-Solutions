b9a57ed77e409f708e246ff83629e0e77076e013
DardanMehmedi <123114133+DardanMehmedi@users.noreply.github.com>
2023-11-07 22:24:00 +0100

    Update 13.cpp

--- Exercises (for the first partial exam)/13.cpp
+++ Exercises (for the first partial exam)/13.cpp
@@ -1,5 +1,16 @@
+/* * 
+*Sweet number is number that contains only even digits (0, 2, 4, 6, 8).
+*In given range (m and n read from SI) find the smallest "sweet number".
+*If there is no such number, print NSN.
+*
+*For example:
+*
+*Input:    /    Result:
+*1000 5000 /    2000
+* */
 #include <iostream>
 using namespace std;
+
 
 int main(){ //smoking kills
     int a, b;
