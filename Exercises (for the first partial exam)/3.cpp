01ed972a6e89e81a266f27b17b65108697de0502
Omer Tairi <omer.tairi@intelory.com>
2023-11-07 12:57:22 +0100

    example on how i think files should be saved as

--- Exercises (for the first partial exam)/3.cpp
+++ Exercises (for the first partial exam)/3.cpp
@@ -1,3 +1,19 @@
+/*
+ * Using the characters + and - form a square image with "width" n (n > 2) that represent square
+ * of the chars -, enclosed by a square of chars + (view examples). For given n,
+ * print the appropriate image.
+ * Attention: do not print additional characters as empty spaces, or unnecessary new line...
+ * The image ends with new line char. Do not use any matrix or arrays because n is not limited.
+ * Input: 3
+ * Result:
+    +++
+    +-+
+    +++
+ */
+
+
+//Link: https://courses.finki.ukim.mk/mod/quiz/attempt.php?attempt=464394&cmid=138228&page=2
+
 #include <iostream>
 using namespace std;
 
