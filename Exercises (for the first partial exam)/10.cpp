7d6e1c6e7c1dfce2b3ae8af13fab5ae5df9f1fde
DardanMehmedi <123114133+DardanMehmedi@users.noreply.github.com>
2023-11-07 22:13:35 +0100

    Update 10.cpp

--- Exercises (for the first partial exam)/10.cpp
+++ Exercises (for the first partial exam)/10.cpp
@@ -1,3 +1,21 @@
+/*
+* Write a program in which from the standard input you read one positive integer z,
+* and afterwards you read one after another pairs of integers (a, b).
+* Thereby, the loop should end if you read the pair (0, 0). 
+* The program should calculate how many times the integer z is equal to the sum of the pairs a and b,
+* as well as the percentage of the pairs (a,b) that give sum equal to z (NOTE: the pair (0,0) is not taken into account when doing calculations!).
+
+* For example:
+
+* Input:    /    Result:  
+* 100       /    You entered 2 pairs of numbers that have a sum equal to 100
+* 50 50     /    The percentage of pairs with sum 100 is 50%
+* -50 -50   /
+* -99 199   /
+* 32 98     /
+* 0 0       /
+
+* */
 #include <iostream>
 using namespace std;
 
