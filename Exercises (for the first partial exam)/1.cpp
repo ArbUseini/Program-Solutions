a8b0af009dabdbeb4cb574ed787fc81fe34e072c
DardanMehmedi <123114133+DardanMehmedi@users.noreply.github.com>
2023-11-07 17:13:22 +0100

    Updated 1.cpp

--- Exercises (for the first partial exam)/1.cpp
+++ Exercises (for the first partial exam)/1.cpp
@@ -1,4 +1,4 @@
-/*
+)/*
  *
  * From SI read characters until you read the char !. In these characters are hidden integers
  * (smaller than 100). Write a program that will read all characters and print the sum of all
@@ -9,39 +9,34 @@
  * */
 // Link: https://courses.finki.ukim.mk/mod/quiz/attempt.php?attempt=464394&cmid=138228
 #include <iostream>
-#include <string>
+
 using namespace std;
-/*
 
-I will update it to use char instead of string
+int main() {
+    char bruh;
+    int num = 0, numalt = 0, sum = 0;
+    
+    while(bruh != '!'){
+        cin>> noskipws >>bruh;
 
-*/
-int main(){ //smoking kills
-    string f; // #include <string>
-    getline(cin, f);
-    int sum(0);
-    for (int i = 0; i < f.length(); i++){
-        if (f[i] >= '0' && f[i] <= '9'){
-            int d;
-            if (f[i + 1] >= '0' && f[i + 1] <= '9'){
-                d = (f[i] - '0')*10 + f[i+1]-'0';
-                sum+=d;
-                i++;
+        if(bruh >= '0' && bruh<= '9'){
+
+            num = bruh - '0';
+            if(numalt == 0){
+                numalt = num;
+            }else{
+                num = (numalt*10)+num;
+                numalt = 0;
+
             }
-            else if (f[i+1] == '!'){
-                d = (f[i] - '0');
-                sum+=d;
-                break;
-            }
-            else{
-                d = (f[i] - '0');
-                sum+=d;
-            }
+
+        }else{
+            numalt = 0;
+            sum += num;
+            num = 0;
+
         }
-        else if (f[i] == '!'){
-                break;
-            }
     }
-    cout << sum;
-    return 0;
+
+    cout<<sum;
 }
