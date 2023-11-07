f380261fe46ef57783843b7f073091368a011639
ArbUseini <77902204+ArbUseini@users.noreply.github.com>
2023-11-07 13:22:40 +0100

    Updated and renamed Labs 3_Taxes.cpp to Lab3_Taxes.cpp

---
+++ Lab3 (Monday)/Lab3_Taxes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+This is also correct now
+
+*/
+
+int main() {
+    int percentage1, threshold1; //this is for the first breakpoint of the income
+    int percentage2, threshold2; //this is for the second breakpoint of the income
+    int percentage_left, salary; //this is for the rest of the income
+    double sum(0); // this should be a double because of the percentages
+    cin >> percentage1  >> threshold1 >> percentage2 >> threshold2 >> percentage_left >> salary;
+    threshold2 -= threshold1; // this is to know how much money can be taxed after the first threshold (threshold1)
+    if (salary > 0){ // checking if there is anything to tax
+        if (salary > threshold1){
+            sum += threshold1 * percentage1 / 100.0; // 100.0 because I want a double added to the sum
+            salary -= threshold1;
+        }
+        else{
+            sum += salary * percentage1 / 100.0;
+            salary = 0;
+        }
+    }
+    if (salary > 0){ // checking if there is anything to tax
+        if (salary > threshold2){
+            sum += threshold2 * percentage2 / 100.0; // 100.0 because I want a double added to the sum
+            salary -= threshold2;
+        }
+        else{
+            sum += salary * percentage2 / 100.0;
+            salary = 0;
+        }
+    }
+    if (salary > 0){ // checking if there is anything to tax
+        sum += salary * percentage_left / 100.0; // the rest of the money
+    }
+    cout << sum;
+    return 0;
+}
