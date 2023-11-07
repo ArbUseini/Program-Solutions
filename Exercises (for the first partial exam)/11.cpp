856fa653598dd8bc97890e75fe93260d0e1dfc73
DardanMehmedi <123114133+DardanMehmedi@users.noreply.github.com>
2023-11-07 22:17:10 +0100

    Update 11.cpp

--- Exercises (for the first partial exam)/11.cpp
+++ Exercises (for the first partial exam)/11.cpp
@@ -1,3 +1,28 @@
+/* *
+* Three natural numbers А, B and C are entered from the SI.
+*
+* Write a program that will check if the three entered numbers can represent the values of the angles (in degrees) of a triangle.
+* If the values can represent angles of a triangle print YES in the first line of the standard output followed by type of the triangle (RIGHT, ACUTE or OBTUSE) in the next line.
+* If triangle is not possible with the tree given angles print NO in a single line.
+* 
+* 
+* Example 1:
+* 
+* 60 40 80
+* YES
+* ACUTE
+* 
+* Example 2:
+* 
+* 100 50 30
+* YES
+* OBTUSE
+* 
+* Example 3:
+* 100 50 50
+* NO
+* 
+* */
 #include <iostream>
 using namespace std;
 
