b164471619182db69b3597047310ddf4c081e526
ArbUseini <77902204+ArbUseini@users.noreply.github.com>
2023-12-02 21:29:48 +0000

    restructure + homework added

--- homework/exercises/7/task1.cpp
+++ homework/exercises/7/task1.cpp
@@ -1,13 +1,31 @@
-// It is necessary to find and count all occurrences of the “X” shape composed
-// only of elements with a value of 1. The “X” shape consists of 5 elements with
+// It is necessary to find and count all occurrences of the “+” shape composed
+// only of elements with a value of 1. The “+” shape consists of 5 elements with
 // a value of 1 that are appropriately distributed in the matrix (an element
 // with a value of 1 that has elements with a value of 1 as its diagonal
 // neighbors).
 
 // The dimensions of a matrix and its elements are entered from the standard
-// input. Count all occurrences of the “X” shape within the matrix. Assume that
-// there should be no overlap of elements from two “X” shapes (example 2).
-// Occurrences of the “X” shape are sought from left to right and top to bottom.
+// input. Count all occurrences of the “+” shape within the matrix. Assume that
+// there should be no overlap of elements from two “+” shapes (example 2).
+// Occurrences of the “+” shape are sought from left to right and top to bottom.
+
+// example 1
+// input:
+// 4 5
+// 0 1 2 3 4
+// 1 1 1 0 1
+// 0 1 5 0 0
+// 3 1 2 2 0
+// output: 1
+// example 2
+// input:
+// 5 6
+// 0 1 2 3 4 3
+// 1 1 1 0 1 4
+// 0 1 5 1 1 1
+// 1 1 1 0 1 1
+// 9 1 2 1 4 8
+// output: 2
 
 #include <iostream>
 using namespace std;
